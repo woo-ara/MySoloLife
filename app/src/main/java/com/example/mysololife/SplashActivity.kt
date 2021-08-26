232479af1e268b264d0fc9fa496254297b57691f
woo-ara <war0426@naver.com>
2021-08-26 22:09:51 +0900

    레이아웃 - 인트로 페이지 꾸미기2

--- app/src/main/java/com/example/mysololife/SplashActivity.kt
+++ app/src/main/java/com/example/mysololife/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+
 import com.example.mysololife.auth.IntroActivity
 
 class SplashActivity : AppCompatActivity() {
